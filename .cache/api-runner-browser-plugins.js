41792c21068757504fca8cfdca7c8556f186e4c8
Jeff Leibensperger <jeffrey.leibensperger@gmail.com>
2021-03-21 06:46:21 -0400

    maybe this does it

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,10 @@
+module.exports = [{
+      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
+      options: {"plugins":[],"name":"jeffl-website","short_name":"jeffl","start_url":"/","background_color":"#fff","display":"minimal-ui","icon":"src/static/icon.svg","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"de5e25d22c6e891d01ac22511ddebd3f"},
+    },{
+      plugin: require('../node_modules/gatsby-plugin-offline/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('../gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
